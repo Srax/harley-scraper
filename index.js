f6816400f0576286617919aed2ee7ba5c7395793
srax <thomas_ebsen@live.dk>
2022-03-01 01:10:21 +0100

    fixes

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const baseUrl = "http://harley-davidson.com";
 const htmlEntity = require("html-entities");
 const ObjectsToCsv = require("objects-to-csv");
 
+const keepAliveAgent = new https.Agent({
+    keepAlive: true,
+    maxSockets: Infinity,
+});
+
 const fs = require("fs");
 const request = require("request");
 
@@ -15,92 +20,116 @@ function sleep(ms) {
 }
 
 const saveSparePartsImage = async (sparePartsArr) => {
-    var download = function (uri, filename, directory, callback) {
-        request.head(uri, function (err, body) {
-            if (!fs.existsSync(rootImageDir)) {
-                fs.mkdirSync(rootImageDir);
-            }
-            request(uri)
-                .pipe(fs.createWriteStream(`${rootImageDir}/${filename}`))
-                .on("close", callback);
-        });
-    };
-    for (const item of sparePartsArr) {
-        var i = 1;
-        for (const imageUrl of item.imageUrls) {
-            await sleep(100); // Wait 100 ms between each scrape. This is necessary to not send too many requests at a time
-            var path = require("path");
-            download(
-                `${baseUrl}${imageUrl}`,
-                `${path.parse(imageUrl).base}.png`,
-                `${rootImageDir}/${item.productCode}`,
-                function () {
-                    console.log(`Downloaded: ${imageUrl}`);
+    try {
+        var download = function (uri, filename, directory, callback) {
+            request.head(uri, function (err, body) {
+                if (!fs.existsSync(rootImageDir)) {
+                    fs.mkdirSync(rootImageDir);
                 }
-            );
-            i++;
+                request(uri)
+                    .pipe(fs.createWriteStream(`${rootImageDir}/${filename}`))
+                    .on("close", callback);
+            });
+        };
+        for (const item of sparePartsArr) {
+            var i = 1;
+            for (const imageUrl of item.imageUrls) {
+                await sleep(100); // Wait 100 ms between each scrape. This is necessary to not send too many requests at a time
+                var path = require("path");
+                download(
+                    `${baseUrl}${imageUrl}`,
+                    `${path.parse(imageUrl).base}.png`,
+                    `${rootImageDir}/${item.productCode}`,
+                    function () {
+                        console.log(`Downloaded: ${imageUrl}`);
+                    }
+                );
+                i++;
+            }
         }
+    } catch (error) {
+        console.log(error.message);
     }
 };
 
 const getDataFromApi = async (url) => {
-    return await new Promise((resolve) => {
-        https.get(url, (res) => {
-            let data = [];
-            res.on("data", (chunk) => {
-                data.push(chunk);
-            });
+    try {
+        return await new Promise((resolve) => {
+            https.get(url, keepAliveAgent, (res) => {
+                let data = [];
+                res.on("data", (chunk) => {
+                    data.push(chunk);
+                });
 
-            res.on("error", (e) => {
-                console.error(e);
-            });
+                res.on("error", (e) => {
+                    console.error(e);
+                });
 
-            res.on("end", () => {
-                resolve(JSON.parse(Buffer.concat(data).toString()));
+                res.on("end", () => {
+                    resolve(JSON.parse(Buffer.concat(data).toString()));
+                });
             });
         });
-    });
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 const fetchItemsWithPagination = async (itemsPerRequest, page) => {
-    const url = `https://www.harley-davidson.com/search/?format=json;i=1;locale=da_DK;page=${page};q1=parts;q2=motorcycle-parts;sp_c=${itemsPerRequest};x1=primaryCategoryCode;x2=superCategoryCodes;sp_cs=UTF-8`;
-    const parts = await getDataFromApi(url);
-    const pagination = parts.pagination[0];
-    const pickedPartsArr = parts.resultsets[0].results;
-    const fetchedItemsArr = [];
-    for (const item of pickedPartsArr) {
-        await sleep(50);
-        const extraDetails = await getDataFromApi(
-            `https://www.harley-davidson.com/dk/da/api-commerce/product/${item.baseProductCode}/get-fitment`
-        );
+    try {
+        const url = `https://www.harley-davidson.com/search/?format=json;i=1;locale=da_DK;page=${page};q1=parts;q2=motorcycle-parts;sp_c=${itemsPerRequest};x1=primaryCategoryCode;x2=superCategoryCodes;sp_cs=UTF-8`;
+        const parts = await getDataFromApi(url);
+        const pagination = parts.pagination[0];
+        const pickedPartsArr = parts.resultsets[0].results;
+        const fetchedItemsArr = [];
+        for (const item of pickedPartsArr) {
+            await sleep(100); // Wait 100 ms between each scrape. This is necessary to not send too many requests at a time
+            const extraDetails = await getDataFromApi(
+                `https://www.harley-davidson.com/dk/da/api-commerce/product/${item.baseProductCode}/get-fitment`
+            );
+            let productDetail = "";
+            if (extraDetails) {
+                if (extraDetails.part !== undefined) {
+                    if (extraDetails.part.hdFitmentCopy !== undefined) {
+                        productDetail = extraDetails.part.hdFitmentCopy
+                            ? extraDetails.part.hdFitmentCopy
+                            : "";
+                    }
+                }
+            }
+            let imageUrls = [
+                `${item.primaryThumbnailUrl}`,
+                `${item.hoverThumbnailUrl}`,
+            ].filter((x) => x !== "");
 
-        let imageUrls = [
-            `${item.primaryThumbnailUrl}`,
-            `${item.hoverThumbnailUrl}`,
-        ].filter((x) => x !== "");
+            fetchedItemsArr.push({
+                productCode: item.baseProductCode,
+                name: item.formattedName,
+                description: htmlEntity.decode(item.description),
+                details: productDetail,
+                productUrl: `${baseUrl}${item.pdpProductUrl}`,
+                categoryCode: htmlEntity.decode(item.categoryCode),
+                categoryName: htmlEntity.decode(item.categoryName),
+                imageUrls: imageUrls,
+                date: new Date().toISOString().split("T")[0],
+            });
+            console.log(
+                `Found product: ${item.baseProductCode} - ${item.formattedName}`
+            );
+        }
 
-        fetchedItemsArr.push({
-            productCode: item.baseProductCode,
-            name: item.formattedName,
-            description: htmlEntity.decode(item.description),
-            details: extraDetails.part.hdFitmentCopy,
-            productUrl: `${baseUrl}${item.pdpProductUrl}`,
-            categoryCode: htmlEntity.decode(item.categoryCode),
-            categoryName: htmlEntity.decode(item.categoryName),
-            imageUrls: imageUrls,
-            date: new Date().toISOString().split("T")[0],
-        });
+        return {
+            pagination: {
+                current: pagination.current,
+                next: pagination.next,
+                previous: pagination.previous,
+                last: pagination.last,
+            },
+            results: fetchedItemsArr,
+        };
+    } catch (error) {
+        console.log(error);
     }
-
-    return {
-        pagination: {
-            current: pagination.current,
-            next: pagination.next,
-            previous: pagination.previous,
-            last: pagination.last,
-        },
-        results: fetchedItemsArr,
-    };
 };
 
 const millisToMinutesAndSeconds = (millis) => {
@@ -110,42 +139,57 @@ const millisToMinutesAndSeconds = (millis) => {
 };
 
 const saveDataAsJsonFile = async (data, dirAndFileName) => {
-    return fs.writeFile(
-        `${dirAndFileName}.json`,
-        JSON.stringify(data),
-        function (err) {
-            if (err) throw err;
-        }
-    );
+    try {
+        return fs.writeFile(
+            `${dirAndFileName}.json`,
+            JSON.stringify(data),
+            function (err) {
+                if (err) throw err;
+            }
+        );
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 const saveDataAsCSVFile = async (data, dirAndFileName) => {
-    const csv = new ObjectsToCsv(data);
-    await csv.toDisk(`${dirAndFileName}.csv`);
-    return csv.toString();
+    try {
+        const csv = new ObjectsToCsv(data);
+        await csv.toDisk(`${dirAndFileName}.csv`);
+        return csv.toString();
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 (async () => {
-    console.log("Scraping... Please wait.");
-    var startTime = Date.now();
-    let resultsArr = [];
-    let hasNextPage = true;
-    let page = 1;
-    while (hasNextPage) {
-        const newCall = await fetchItemsWithPagination(100, page);
-        console.log(`Fetching items from page: ${page}`);
+    try {
+        console.log("Scraping... Please wait.");
+        var startTime = Date.now();
+        let resultsArr = [];
+        let hasNextPage = true;
+        let page = 1;
+        while (hasNextPage) {
+            console.log(`Fetching items from page: ${page}`);
+            const newCall = await fetchItemsWithPagination(500, page);
 
-        resultsArr = resultsArr.concat(newCall.results);
-        if (newCall.pagination.next.length <= 2) {
-            hasNextPage = false;
+            resultsArr = resultsArr.concat(newCall.results);
+            if (newCall.pagination.next.length <= 2) {
+                hasNextPage = false;
+            }
+            page++;
         }
-        page++;
-    }
 
-    await saveDataAsJsonFile(resultsArr, "scraped_parts_json");
-    await saveDataAsCSVFile(resultsArr, "scraped_parts_csv");
-    await saveSparePartsImage(resultsArr);
-    let elapsed = Date.now() - startTime;
-    await sleep(1500);
-    console.log("Finished in", millisToMinutesAndSeconds(elapsed) + " minutes");
+        await saveDataAsJsonFile(resultsArr, "scraped_parts_json");
+        await saveDataAsCSVFile(resultsArr, "scraped_parts_csv");
+        await saveSparePartsImage(resultsArr);
+        let elapsed = Date.now() - startTime;
+        await sleep(3000);
+        console.log(
+            "Finished in",
+            millisToMinutesAndSeconds(elapsed) + " minutes"
+        );
+    } catch (error) {
+        console.log(error.message);
+    }
 })();
